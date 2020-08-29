0881c250e7c2ae532fb78731ad0c2c1c0f1b50e4
li.yunhao <li.yunhao@foxmail.com>
2020-08-29 13:32:53 +0800

    COUTER+MUX5+DECODER+FUN2+PC+LS273+CONVERT+ROM+MUX4+ALU+PSW

--- alu.vhd
+++ alu.vhd
@@ -1,58 +1,64 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
-USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.STD_LOGIC_SIGNED.ALL;
 ENTITY ALU IS
 PORT(
-	A:IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-	B:IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-	S1,S0:IN STD_LOGIC;
-	BCDOUT:OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-	CF,ZF:OUT STD_LOGIC
-);
+     X: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+     Y: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+	 S2,S1,S0: IN STD_LOGIC;
+     ALUOUT: OUT STD_LOGIC_VECTOR(7 DOWNTO 0) ;
+     CF,ZF: OUT STD_LOGIC
+    );
 END ALU;
 ARCHITECTURE A OF ALU IS
 SIGNAL AA,BB,TEMP:STD_LOGIC_VECTOR(8 DOWNTO 0);
-BEGIN
-	PROCESS
-	BEGIN
-		IF(S1='0'AND S0='0')THEN
-			AA<='0'&A;
-			BB<='0'&B;
-			TEMP<=AA+BB;
-			BCDOUT<=TEMP(7 DOWNTO 0);
-			CF<=TEMP(8);
-			IF(TEMP="100000000"OR TEMP="000000000")THEN
-				ZF<='1';
-			ELSE
-				ZF<='0';
-			END IF;
-		ELSIF(S1='0'AND SO='1')THEN
-			BCDOUT<=A-B;
-			IF(A<B)THEN
-				CF<='1';
-				ZF<='0';
-			ELSIF(A=B)THEN
-				CF<='0';
-				ZF<='1';
-			ELSE
-				CF<='0';
-				ZF<='0';
-			END IF;
-		ELSIF(S1='1' AND SO='0')THEN
-			AA<='0'&A;
-			TEMP<=A+1;
-			BCDOUT<=TEMP(7 DOWNTO 0);
-			CF<=TEMP(8);
-			IF(TEMP="100000000")THEN
-				ZF<='1';
-			ELSE
-				ZF<='0';
-			END IF;
+SIGNAL TEMP1:STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL TEMP2:STD_LOGIC_VECTOR(15 DOWNTO 0);
+BEGIN 
+    PROCESS
+	BEGIN 
+        IF(S2='0' AND S1='0' AND S0='0') THEN	--ADD
+            AA<='0'&X;
+            BB<='0'&Y;
+            TEMP<=AA+BB;
+			ALUOUT<=TEMP(7 DOWNTO 0);
+            CF<=TEMP(8);
+            IF (TEMP="100000000" OR TEMP="000000000") THEN
+                ZF<='1';
+            ELSE
+                ZF<='0';
+            END IF;
+		ELSIF(S2='0' AND S1='0' AND S0='1') THEN    --TEST
+			CF<=X(7);
+			IF (X="00000000") THEN
+                ZF<='1';
+            ELSE
+                ZF<='0';
+            END IF;
+		
+        ELSIF(S2='0' AND S1='1' AND S0='1') THEN     --DEC
+            AA<='0'&Y;
+            TEMP<=AA-1;
+ 			ALUOUT<=TEMP(7 DOWNTO 0);
+            CF<=TEMP(8);
+            IF (TEMP="000000000") THEN
+                ZF<='1';
+            ELSE
+                ZF<='0';
+            END IF;
+		ELSIF(S2='1' AND S1='0' AND S0='0') THEN     --MUL
+            TEMP2<=X * Y;
+ 			ALUOUT<=TEMP2(7 DOWNTO 0);
+         
+        ELSIF(S2='1' AND S1='1' AND S0='0') THEN     --Rd->BUS
+ 			ALUOUT<=Y;
 		ELSE
-			BCDOUT<="00000000";
-			CF<='0';
-			ZF<='0';
-		END IF;
-	END PROCESS;
+		    ALUOUT<="00000000" ;
+		    CF<='0';
+            ZF<='0';
+        END IF;
+    END PROCESS;
 END A;
+
+
--- counter.vhd
+++ counter.vhd
@@ -2,30 +2,31 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 ENTITY COUNTER IS
 PORT(
-	Q,CLR:IN STD_LOGIC;
-	T1,T2,T3,T4:OUT STD_LOGIC
-	);
+     CLK,CLR: IN STD_LOGIC;
+     T2,T3,T4: OUT STD_LOGIC
+    );
 END COUNTER;
+
 ARCHITECTURE A OF COUNTER IS
-SIGNAL X:STD_LOGIC_VECTOR(1 DOWNTO 0);
+SIGNAL X:STD_LOGIC_VECTOR(1 DOWNTO 0):="00";
 BEGIN
-	PROCESS(Q,CLR)
-	BEGIN
-		IF(CLR='0')THEN
-			T1<='0';
-			T2<='0';
-			T3<='0';
-			T4<='0';
-			X<="00";
-		ELSIF(Q'EVENT AND Q='1')THEN
-			X<=X+1;
-			T1<=(NOT X(1)) AND (NOT X(0));
-			T2<=(NOT X(1)) AND X(0);
-			T3<=X(1) AND (NOT X(0));
-			T4<=X(1) AND X(0);
-		END IF;
-	END PROCESS;
+     PROCESS(CLK,CLR)
+     BEGIN
+          IF(CLR='0') THEN
+               T2<='0';
+               T3<='0';
+               T4<='0';
+               X<="00";
+          ELSIF(CLK'EVENT AND CLK='1') THEN
+               X<=X+1;
+               T2<=(NOT X(1))AND X(0);
+               T3<=X(1) AND(NOT X(0));
+               T4<=X(1) AND X(0);
+          END IF;
+     END PROCESS;
 END A;
+
 
