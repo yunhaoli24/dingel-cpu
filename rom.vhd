5fc3ae9ac7dcd9ceb1563a0d687249ca5f09b82b
li.yunhao <li.yunhao@foxmail.com>
2020-09-09 22:35:39 +0800

    更新指令注记符

--- rom.vhd
+++ rom.vhd
@@ -11,17 +11,22 @@ PORT(
 END ROM;
 ARCHITECTURE A OF ROM IS
 
---  注记符号    指令格式(OP)    Rs Rd     addr   
---    MOV            0001       XX Rd    XXXXXXXX
---    INT            0010       XX Rd    XXXXXXXX
---    TEST           0011       Rs XX    XXXXXXXX
---    JB             0100       XX XX     addr
---    MUL            0101       Rs Rd    XXXXXXXX
---    ADD            0110       Rs Rd    XXXXXXXX
---    DEC            0111       XX Rd    XXXXXXXX
---    JZ             1000       XX XX     addr
---    OUT            1001       Rs XX    XXXXXXXX
---    JMP            1010       XX XX     addr
+--  注记符号    指令格式(OP)    Rs Rd     addr
+   
+--    MOV          000001       XX Rd     im
+--    IN1          000010       XX Rd    XXXXXXXX
+--    DEC          000011       XX Rd    XXXXXXXX
+--    TEST         000100       XX Rd     addr
+--    JB           000101       XX Rd     addr
+--    JZ           000110       XX Rd     addr
+--    JMP          000111       XX XX     addr
+--    MUL          001000       Rs Rd    XXXXXXXX
+--    ADD          001001       Rs Rd    XXXXXXXX
+--    OUT          001010       Rs XX    XXXXXXXX
+--    LAD          001011       Rs Rd    XXXXXXXX
+--    STO          001101       Rs Rd    XXXXXXXX
+--    INC          001111       XX Rd    XXXXXXXX
+--    CMP          010000       Rs Rd    XXXXXXXX
 BEGIN
 	DOUT<="0001000100000010" WHEN ADDR="00000000" AND CS_I='0' ELSE--MOV R1,2H
           "0001001000000110" WHEN ADDR="00000001" AND CS_I='0' ELSE--MOV R2,6H
